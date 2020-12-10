ddb626aa5c3fe6abc9fc5588aef4986438d337f7
Vladimir <riuson@gmail.com>
2020-12-10 11:32:30 +0500

    Add implementation for filling board

--- ChessBoard.Lib/Shared/Board.cs
+++ ChessBoard.Lib/Shared/Board.cs
@@ -2,6 +2,8 @@
 
 namespace ChessBoard.Lib.Shared {
     public class Board {
+        private Figure[,] _figures;
+
         public IBoardDrawer Drawer { get; set; }
 
         public int HCells { get; private set; }
@@ -10,6 +12,18 @@ namespace ChessBoard.Lib.Shared {
         public void Initialize(BoardInitializationData initializationData) {
             this.HCells = initializationData.HCells;
             this.VCells = initializationData.VCells;
+
+            this._figures = new Figure[initializationData.HCells, initializationData.VCells];
+
+            for (var x = 0; x < initializationData.HCells; x++) {
+                for (var y = 0; y < initializationData.VCells; y++) {
+                    this._figures[x, y] = Figure.Empty;
+                }
+            }
+
+            foreach (var item in initializationData.Figures) {
+                this._figures[item.X, item.Y] = item.Figure;
+            }
         }
 
         public void Create() {
@@ -22,8 +36,20 @@ namespace ChessBoard.Lib.Shared {
             drawer.Draw();
         }
 
-        public Figure GetFigureAt(int x, int y) => throw new NotImplementedException();
+        public Figure GetFigureAt(int x, int y) => this._figures[x, y];
 
-        public IEnumerable<FigureAtPosition> GetFiguresOnBoard() => throw new NotImplementedException();
+        public IEnumerable<FigureAtPosition> GetFiguresOnBoard() {
+            var result = new List<FigureAtPosition>();
+
+            for (var x = 0; x < this.HCells; x++) {
+                for (var y = 0; y < this.VCells; y++) {
+                    if (!this._figures[x, y].IsEmpty) {
+                        result.Add(new FigureAtPosition(this._figures[x, y], x, y));
+                    }
+                }
+            }
+
+            return result;
+        }
     }
 }
--- ChessBoard.Lib/Shared/BoardInitializationData.cs
+++ ChessBoard.Lib/Shared/BoardInitializationData.cs
@@ -7,7 +7,29 @@ namespace ChessBoard.Lib.Shared {
 
         public virtual IEnumerable<FigureAtPosition> Figures {
             get {
-                throw new NotImplementedException();
+                var result = new List<FigureAtPosition>();
+
+                for (var x = 0; x < this.HCells; x++) {
+                    result.Add(new FigureAtPosition(new Figure(FigureType.Pawn, Side.Black), x, 1));
+                }
+
+                var row0 = new[] {
+                    FigureType.Rook,
+                    FigureType.Knight,
+                    FigureType.Bishop,
+                    FigureType.Queen,
+                    FigureType.King,
+                    FigureType.Bishop,
+                    FigureType.Knight,
+                    FigureType.Rook
+                };
+
+                for (var x = 0; x < row0.Length; x++) {
+                    result.Add(new FigureAtPosition(new Figure(row0[x], Side.Black), x, 0));
+                    result.Add(new FigureAtPosition(new Figure(row0[x], Side.White), x, 7));
+                }
+
+                return result;
             }
         }
     }
